f568f8e650ae1cd67a6c584b76e3c900db4a2aec
Simon Eisenmann <simon@longsleep.org>
2014-03-17 01:42:38 +0100

    Implemented page loader.

---
+++ static/js/directives/page.js
@@ -0,0 +1,65 @@
+/*
+ * Spreed Speak Freely.
+ * Copyright (C) 2013-2014 struktur AG
+ *
+ * This file is part of Spreed Speak Freely.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+define(['text!partials/page.html', 'text!partials/page/welcome.html'], function(template, welcome) {
+
+	return ["$templateCache", function($templateCache) {
+
+		$templateCache.put('page/welcome.html', welcome);
+
+		var link = function(scope, element, attrs) {
+
+			scope.room = false;
+			scope.page = null;
+
+			scope.$on("welcome", function() {
+				if (!scope.initialized) {
+					scope.initialized = true;
+					scope.refresh();
+				}
+			});
+
+			scope.$on("room", function(event, room) {
+				console.log("aaa roomStatus", room !== null ? true : false);
+				scope.initialized = true;
+				scope.room = room !== null ? true : false;
+				scope.refresh();
+			});
+
+			scope.refresh = function() {
+				if (scope.room) {
+					scope.page = null;
+				} else {
+					scope.page = "page/welcome.html";
+				}
+			};
+
+		};
+
+		return {
+			restrict: 'E',
+			replace: true,
+			template: template,
+			link: link
+		}
+
+	}];
+
+});
