60ee57edcf4e3ba9086b34e238c711a741865e41
Simon Eisenmann <simon@struktur.de>
2014-03-18 18:45:57 +0100

    Show something during initializing phase and use new style css layout for help.

--- static/js/directives/page.js
+++ static/js/directives/page.js
@@ -49,7 +49,7 @@ define(['text!partials/page.html', 'text!partials/page/welcome.html'], function(
 			});
 
 			scope.refresh = function() {
-				if (scope.room || scope.status !== "waiting") {
+				if (scope.roomid || scope.room || scope.status !== "waiting") {
 					scope.page = null;
 				} else {
 					scope.page = "page/welcome.html";
