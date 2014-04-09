4cbf20291af8e68bead4aa24410ddbdb2388148c
Simon Eisenmann <simon@struktur.de>
2014-04-09 17:16:57 +0200

    Updated enabled languages.

--- static/translation/languages.js
+++ static/translation/languages.js
@@ -1,4 +1,4 @@
 // This file is auto generated, do not modify.
 define([], function() {
-return {"zh-cn": "\u4e2d\u6587", "de": "Deutsch", "en": "English", "ko": "\ud55c\uad6d\uc5b4"};
+return {"en": "English", "zh-tw": "\u7e41\u9ad4\u4e2d\u6587", "de": "Deutsch", "ko": "\ud55c\uad6d\uc5b4", "zh-cn": "\u4e2d\u6587\uff08\u7b80\u4f53\uff09", "ja": "\u65e5\u672c\u8a9e"};
 });
