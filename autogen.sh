b49a09ac7bd9386f5cf142f0762dc1cd8fcaa01c
Joachim Bauch <bauch@struktur.de>
2014-05-23 17:02:51 +0200

    Added autoconf/automake support. This way we can check for required dependencies and minimum versions. No functional changes have been done to the Makefile yet.

---
+++ autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# the pre-commit hook performs various formatting checks
+if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \); then \
+    rm -f .git/hooks/pre-commit; \
+    ln -s ../../src/hooks/pre-commit.hook .git/hooks/pre-commit; \
+fi
+
+if [ -x "`which autoreconf 2>/dev/null`" ] ; then
+   exec autoreconf -ivf
+fi
+
+LIBTOOLIZE=libtoolize
+SYSNAME=`uname`
+if [ "x$SYSNAME" = "xDarwin" ] ; then
+  LIBTOOLIZE=glibtoolize
+fi
+aclocal -I m4 && \
+	autoheader && \
+	$LIBTOOLIZE && \
+	autoconf && \
+	automake --add-missing --force-missing --copy
