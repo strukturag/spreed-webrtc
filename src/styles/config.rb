3a07f38f1fa51013be2c97372024b62d3884e84e
Simon Eisenmann <simon@struktur.de>
2014-08-29 14:43:22 +0200

    Fixed compatibility with Sass 3.4 (3.4 is now required).

--- src/styles/config.rb
+++ src/styles/config.rb
@@ -1,4 +1,6 @@
-images_dir = "../../static/img"
 project_path = "../../static/css"
+images_dir = "../../static/img"
+sass_dir = "../../static/css"
+fonts_dir = "../../static/fonts"
 
 relative_assets = true
