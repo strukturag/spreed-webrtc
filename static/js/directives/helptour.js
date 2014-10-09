cc13ad2bcb592b7618b9e49f3f18350b3397da98
Evan Theurer <theurer@struktur.de>
2014-10-09 14:27:54 +0200

    Add a tour controller.

--- static/js/directives/helptour.js
+++ static/js/directives/helptour.js
@@ -24,20 +24,45 @@ define(['jquery', 'text!partials/helpoverlay.html', 'text!partials/helptour.html
 	//helptour
 	return [function() {
 
-		var controller = ['$scope', '$timeout', '$modal', 'userSettingsData', function($scope, $timeout, $modal, userSettingsData) {
-			var displayTime = 500;
+		var controller = ['$scope', '$timeout', '$modal', 'userSettingsData', '$q', function($scope, $timeout, $modal, userSettingsData, $q) {
+			var displayTime = 2000;
 			var shown = localStorage.getItem('mediastream-helptour');
-			var doStep = function(i, elem) {
-				$timeout(function() {
-					$(elem).addClass('in');
+			var timeoutAutoStepsIn = [];
+			var timeoutAutoStepsOut = [];
+			var reset = function() {
+				$scope.toAutourPaused = false;
+				$($scope.steps[$scope.currentIndex]).removeClass('in');
+				$scope.currentIndex = null;
+			};
+			var outStep = function(i, elem) {
+				$(elem).removeClass('in');
+			};
+			var inStep = function(i, elem) {
+				$(elem).addClass('in');
+				$scope.currentIndex = i;
+			};
+			var autoStep = function(i, elem) {
+				timeoutAutoStepsIn[i] = $timeout(function() {
+					inStep(i, elem);
 				}, displayTime * i, true);
-				$timeout(function() {
-					$(elem).removeClass('in');
+				timeoutAutoStepsOut[i] = $timeout(function() {
+					outStep(i, elem);
 				}, displayTime * (i + 1), true);
 			};
-			var startTour = function() {
+			var autoTour = function(start) {
+				start = start ? start - 1 : 0;
 				$scope.steps.each(function(i, x) {
-					doStep(i, x);
+					if(i >= start) {
+						autoStep(i, x);
+					}
+				});
+			};
+			var manualTour = function() {
+				timeoutAutoStepsIn.forEach(function(promise) {
+					$timeout.cancel(promise);
+				});
+				timeoutAutoStepsOut.forEach(function(promise) {
+					$timeout.cancel(promise);
 				});
 			};
 			var introTour = function() {
@@ -45,7 +70,7 @@ define(['jquery', 'text!partials/helpoverlay.html', 'text!partials/helptour.html
 				var controller = ['$scope', '$modalInstance', function(scope, $modalInstance) {
 					scope.goTour = function() {
 						$modalInstance.dismiss();
-						startTour();
+						autoTour();
 					};
 				}];
 				$modal.open({
@@ -58,15 +83,43 @@ define(['jquery', 'text!partials/helpoverlay.html', 'text!partials/helptour.html
 				introTour();
 				localStorage.setItem('mediastream-helptour', true);
 			}
-			$scope.$on('showHelpTour', function(current, last) {
-				if(current) {
-					introTour();
+			$scope.tourPaused = false;
+			$scope.currentIndex = null;
+			$scope.stepBeginning = function() {
+				outStep($scope.currentIndex, $scope.steps[$scope.currentIndex]);
+				inStep(0, $scope.steps[0]);
+			};
+			$scope.stepBackward = function() {
+				outStep($scope.currentIndex, $scope.steps[$scope.currentIndex]);
+				inStep($scope.currentIndex - 1, $scope.steps[$scope.currentIndex - 1]);
+			};
+			$scope.stepForward = function() {
+				outStep($scope.currentIndex, $scope.steps[$scope.currentIndex]);
+				inStep($scope.currentIndex + 1, $scope.steps[$scope.currentIndex + 1]);
+			};
+			$scope.stepEnd = function() {
+				outStep($scope.currentIndex, $scope.steps[$scope.currentIndex]);
+				inStep($scope.steps.length - 1, $scope.steps[$scope.steps.length - 1]);
+			};
+			$scope.togglePause = function() {
+				$scope.tourPaused = !$scope.tourPaused;
+				if ($scope.tourPaused) {
+					manualTour();
+				} else {
+					autoTour($scope.currentIndex);
 				}
+			};
+			$scope.exitTour = function() {
+				manualTour();
+				reset();
+			};
+			$scope.$on('showHelpTour', function() {
+				introTour();
 			});
 		}];
 
 		var link = function($scope, $elem, $attrs) {
-			$scope.steps = $elem.find('.popover');
+			$scope.steps = $elem.find('.tourSteps .popover');
 		};
 
 		return {
