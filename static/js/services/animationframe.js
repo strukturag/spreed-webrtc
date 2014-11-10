b9733d08efbbf0159039e7f1effea3dc23ae01a4
Simon Eisenmann <simon@struktur.de>
2014-11-10 19:42:04 +0100

    Reduced animationFrame rate to 10fps (down from 60).

--- static/js/services/animationframe.js
+++ static/js/services/animationframe.js
@@ -29,9 +29,13 @@ define(["underscore", "rAF"], function(_) {
 		var caller = function(f) {
 			f();
 		};
+		var runner = function(c) {
+			registry.forEach(caller);
+			requestAnimationFrame(worker)
+		}
+		var timer = $window.setTimeout;
 		var worker = function() {
-			registry.forEach(caller)
-			requestAnimationFrame(worker);
+			timer(runner, 100);
 		};
 
 		// Public api.
