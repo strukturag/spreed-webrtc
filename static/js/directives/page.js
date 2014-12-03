ab48ccc0d0415fbbb2f56e2ddeda9057fbb48874
Simon Eisenmann <simon@struktur.de>
2014-12-03 16:19:30 +0100

    Focus input field instead of button.

--- static/js/directives/page.js
+++ static/js/directives/page.js
@@ -36,7 +36,7 @@ define(['text!partials/page.html', 'text!partials/page/welcome.html'], function(
 				$scope.page = "page/welcome.html";
 				$scope.roomdata = roomdata;
 				$timeout(function() {
-					$element.find(".btn-roomcreate:visible:enabled:first").focus();
+					$element.find(".roomdata-link-input:visible:enabled:first").focus();
 				});
 			});
 
