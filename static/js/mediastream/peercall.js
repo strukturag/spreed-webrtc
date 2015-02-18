c578fbb460767705533898798e4dad9a828cfac3
Simon Eisenmann <simon@struktur.de>
2015-02-18 11:51:02 +0100

    Added callbacks to addIceCandidate function to comply with WebRTC API.

--- static/js/mediastream/peercall.js
+++ static/js/mediastream/peercall.js
@@ -243,7 +243,11 @@ define(['jquery', 'underscore', 'mediastream/utils', 'mediastream/peerconnection
 			// Avoid errors when still receiving candidates but closed.
 			return;
 		}
-		this.peerconnection.addIceCandidate(candidate);
+		this.peerconnection.addIceCandidate(candidate, function() {
+			//console.log("Remote candidate added successfully.", candidate);
+		}, function(error) {
+			console.warn("Failed to add remote candidate:", error, candidate);
+		});
 
 	};
 
