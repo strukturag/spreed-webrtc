9e3257c3bb9da9e6d6d92d9416e879c1a010356e
Joachim Bauch <mail@joachim-bauch.de>
2015-08-08 01:36:41 +0200

    Add new encryption service using libaxolotl-javascript.

--- static/js/services/services.js
+++ static/js/services/services.js
@@ -70,7 +70,8 @@ define([
 	'services/mediadevices',
 	'services/sandbox',
 	'services/dummystream',
-	'services/usermedia'], function(_,
+	'services/usermedia',
+	'services/encryption'], function(_,
 desktopNotify,
 playSound,
 safeApply,
@@ -118,7 +119,8 @@ modules,
 mediaDevices,
 sandbox,
 dummyStream,
-userMedia) {
+userMedia,
+encryption) {
 
 	var services = {
 		desktopNotify: desktopNotify,
@@ -168,7 +170,8 @@ userMedia) {
 		mediaDevices: mediaDevices,
 		sandbox: sandbox,
 		dummyStream: dummyStream,
-		userMedia: userMedia
+		userMedia: userMedia,
+		encryption: encryption
 	};
 
 	var initialize = function(angModule) {
