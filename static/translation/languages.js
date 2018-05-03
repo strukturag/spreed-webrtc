57c7dad04dfd1debbcaab68915c48ed7872e7dae
nunojeronimo <38939858+nunojeronimo@users.noreply.github.com>
2018-05-03 12:31:17 +0100

    Update languages.js
    Add Portuguese

--- static/translation/languages.js
+++ static/translation/languages.js
@@ -1,5 +1,5 @@
 // This file is auto generated, do not modify.
 "use strict";
 define([], function() {
-return {"zh-cn": "\u4e2d\u6587\uff08\u7b80\u4f53\uff09", "fr": "Fran\u00e7ais", "en": "English", "ru": "\u0420\u0443\u0441\u0441\u043a\u0438\u0439", "zh-tw": "\u7e41\u9ad4\u4e2d\u6587", "de": "Deutsch", "ko": "\ud55c\uad6d\uc5b4", "ja": "\u65e5\u672c\u8a9e", "es": "Espa\u00f1ol", "it": "Italiano"};
+return {"zh-cn": "\u4e2d\u6587\uff08\u7b80\u4f53\uff09", "fr": "Fran\u00e7ais", "en": "English", "ru": "\u0420\u0443\u0441\u0441\u043a\u0438\u0439", "zh-tw": "\u7e41\u9ad4\u4e2d\u6587", "de": "Deutsch", "ko": "\ud55c\uad6d\uc5b4", "ja": "\u65e5\u672c\u8a9e", "es": "Espa\u00f1ol", "it": "Italiano", "pt": "Portugu\u00eas"};
 });
